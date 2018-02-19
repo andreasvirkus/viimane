1234b59f779e2c9b57bf4395f541c794e8be6137
antc <write@andreasvirkus.me>
2018-02-19 22:31:46 +0200

    Add cousine font, navbar component and landing page SFC

---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <header>
+    <!-- <Logo /> -->
+    <router-link to="/">viimane</router-link>
+
+    <nav>
+      <router-link to="/buss">buss</router-link>
+      <router-link to="/tramm">tramm</router-link>
+      <router-link to="/troll">troll</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+  import Logo from '@/assets/svg/logo.svg'
+
+  export default {
+    name: 'navbar',
+    components: { Logo }
+  }
+</script>
+
+<style>
+  header {
+    display: flex;
+    margin: 0;
+    height: 56px;
+    padding: 0 16px 0 24px;
+    background-color: #35495E;
+    color: #ffffff;
+    /* box-shadow: 0 16px 24px 0 rgba(118,143,255,0.2); */
+  }
+
+  header > a {
+    display: block;
+    position: relative;
+    font-size: 20px;
+    line-height: 1;
+    letter-spacing: .02em;
+    font-weight: 400;
+    box-sizing: border-box;
+    padding-top: 16px;
+    margin-right: auto;
+    text-decoration: none;
+  }
+
+  header a {
+    margin-left: 1em;
+  }
+
+  header nav {
+    display: flex;
+    align-items: center;
+  }
+</style>
---
+++ src/pages/Landing.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h2>Hei-hopsti!</h2>
+
+    <p>Allpool näed vastava ühistranspordi viimaseid aegu,
+      et teaksid, millal jope selga ajada või mitu klaasi veel
+      rüübata jõuab!</p>
+
+      <p>Turvalist liiklemist!</p>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'landing'
+  }
+</script>
+
+<style>
+  section {
+    padding: 40px;
+    color: rgb(17,17,17);
+    font-weight: 200;
+    max-width: 740px;
+    /* width: 90%; */
+    margin: 10px auto 45px;
+    background-color: #fff;
+    box-shadow: 0 16px 24px 0 rgba(118,143,255,0.2);
+  }
+</style>
