274d41afd8a9566d92c591a659c244899a833287
andreas virkus <andreas.virkus@bigbank.ee>
2018-02-21 11:57:05 +0200

    Change stop param from number to name

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,7 @@ export default new Router({
       component: Trolley
     },
     {
-      path: '/peatus/:number?',
+      path: '/peatus/:name?',
       name: 'Stop',
       component: Stop
     }
