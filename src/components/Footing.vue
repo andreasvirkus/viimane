3974a00693be298e3b9f037d65531ca1665560ad
antc <write@andreasvirkus.me>
2018-03-11 12:42:44 +0200

    Switch commons icon for github, link to source

--- src/components/Footing.vue
+++ src/components/Footing.vue
@@ -1,16 +1,18 @@
 <template>
   <footer class="footer">
-    <CreativeCommons class="footer-icon" />
-    <a href="https://andreasvirkus.me" class="footer-link">ajv</a> /2018
+    <a title="See the source code" href="https://github.com/andreasvirkus/viimane" class="footer-link">
+      <SourceIcon class="footer-icon" />
+    </a>
+    <a title="Visit my personal site" href="https://andreasvirkus.me" class="footer-link">ajv</a> /2018
   </footer>
 </template>
 
 <script>
-  import CreativeCommons from '@/assets/svg/ccommons.svg'
+  import SourceIcon from '@/assets/svg/github.svg'
 
   export default {
     name: 'footing',
-    components: { CreativeCommons }
+    components: { SourceIcon }
   }
 </script>
 
@@ -21,8 +23,12 @@
     padding: 2em 0;
   }
   .footer-link {
-    color: #ff3d00;
+    border-color: #ff3d00;
   }
+  .footer-link:hover {
+    border-color: inherit;
+  }
+
   .footer-icon {
     height: 2em;
     transform: translateY(11px);
